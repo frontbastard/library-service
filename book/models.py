546a167ad874cf66521e3ead28a67d4232ff77b9
pavlo <frontbastard@gmail.com>
2024-08-09 15:00:00 +0200

    Create MVP for the books service
    - Created models, serializers, views and urls for Book
    - Connected the app to the INSTALLED_APPS in settings.py
    - Included the app urls.py to the main service urls.py file
    - Created the new migrations file

--- book/models.py
+++ book/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "hard", "Hard"
+        SOFT = "soft", "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=10,
+        choices=CoverChoices.choices
+    )
+    inventory = models.IntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.title
