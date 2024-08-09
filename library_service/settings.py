d2c32fd10682a3bf2222027e1445ae2ea01745f8
pavlo <frontbastard@gmail.com>
2024-08-09 22:10:39 +0200

    Create user permissions
    - Create IsAdminOrReadOnly permission
    - Add this permission as DEFAULT_PERMISSION_CLASSES in the settings.py file

--- library_service/settings.py
+++ library_service/settings.py
@@ -139,7 +139,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "book.permissions.IsAdminOrReadOnly",
     ],
 }
 
