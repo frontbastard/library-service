6e08588fe538b014b19da1997c911c0cd32aa182
pavlo <frontbastard@gmail.com>
2024-08-10 14:15:31 +0200

    Create borrowing model
    - Created model for the `borrowing` service
    - Made migrations

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from book.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return (f"Borrowing: {self.book.title} by "
+                f"{self.user.email} on {self.borrow_date}")
+
+    class Meta:
+        ordering = ("-borrow_date",)
