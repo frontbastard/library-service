d6e688bbae4ab4fa36b69863e636434003213a80
pavlo <frontbastard@gmail.com>
2024-08-10 14:13:44 +0200

    Create Borrowing service
    - Started new app `borrowing`
    - Created an empty borrowing/urls.py and added it to the project's urls.py file
    - Extended INSTALLED_APPS with the `borrowing` app

--- library_service/urls.py
+++ library_service/urls.py
@@ -22,6 +22,10 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/book/", include("book.urls", namespace="book")),
+    path(
+        "api/borrowing/",
+        include("borrowing.urls", namespace="borrowing")
+    ),
     path("api/user/", include("user.urls", namespace="user")),
 ]
 
