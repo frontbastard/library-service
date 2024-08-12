8da9059bc2cc9ebf85bf4973ab1c1785047e360d
pavlo <frontbastard@gmail.com>
2024-08-12 23:30:46 +0200

    [LS-9] Add entities to admin.py
    https://trello.com/c/lZrQaw4u/9-add-entities-to-admin-panel

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from book.models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "inventory", "daily_fee", "cover")
